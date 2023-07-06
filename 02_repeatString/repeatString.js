7f1d44bbd47a2d5614191c0657a368d395fe6146
Manuel Beltran <136205896+linkobe824@users.noreply.github.com>
2023-07-05 18:21:13 -0700

    finish 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(s, n) {
+    let repeated = "";
+    if(n < 0){
+        return "ERROR"
+    }
+    for(let i = 0; i < n; i++){
+        repeated += s;
+    }
+    return repeated;
 };
 
 // Do not edit below this line
