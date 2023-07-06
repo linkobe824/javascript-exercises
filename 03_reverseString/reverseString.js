8d214a5ac89cc92783dac19c304af5bd3ab4d53f
Manuel Beltran <136205896+linkobe824@users.noreply.github.com>
2023-07-05 18:38:31 -0700

    finish 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    const arrayOfString = s.split('');
+    let reversedArray = []
+    for(let i = arrayOfString.length - 1; i >= 0; i-- ){
+        reversedArray.push(arrayOfString[i]);
+    }
+    return reversedArray.join('');
 };
 
 // Do not edit below this line
