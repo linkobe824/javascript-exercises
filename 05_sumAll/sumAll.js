4e74fb72eb84fe603994971cfb91e6583360dc84
Manuel Beltran <136205896+linkobe824@users.noreply.github.com>
2023-07-05 19:29:17 -0700

    finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(
+        (typeof(num1) != "number" || typeof(num2) != "number") ||
+        (num1 < 0 || num2 < 0)) return 'ERROR'
+       
+    let start = num1;
+    let end = num2;
+
+    if(end < start){
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+    let total = 0;
+    for(let i = start; i <= end; i++){  
+        total += i;
+    }
+    return total;
 
 };
 
